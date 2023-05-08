1d1d859a704118705018703011ca93d497913a3a
IsabellaAlvarezHincapie <123569257+IsabellaAlvarezHincapie@users.noreply.github.com>
2023-05-08 11:59:50 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempoFaltante = document.querySelector("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoActual = tiempoFaltante.textContent;
