f5d8402e401ce3241a2f5a524b1baef4622f154c
IsabellaAlvarezHincapie <123569257+IsabellaAlvarezHincapie@users.noreply.github.com>
2023-05-15 11:55:18 -0500

    js

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoActual = tiempoFaltante.textContent;
+
+function cuadradoAzar {
+  cuadrado.forEach (nombreDeClase => {
+    nombreDeClase.classList.remove("topo");
+  })
+} 
