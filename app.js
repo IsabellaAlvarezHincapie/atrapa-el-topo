f980079fb58debb1b934668b872f3f8d7e9519b8
IsabellaAlvarezHincapie <123569257+IsabellaAlvarezHincapie@users.noreply.github.com>
2023-06-07 10:13:38 -0500

    topo mueve

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const cuadrado = document.querySelectorAll(".cuadrado");
-const tiempoFaltante = document.querySelector("#tiempo");
+const tiempofaltante = document.querySelector("#tiempo");
 let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
-let tiempoActual = tiempoFaltante.textContent;
+let tiempoactual = tiempofaltante.textContent;
+let posiciontopo = null;
 
-function cuadradoAzar {
-  cuadrado.forEach (nombreDeClase => {
-    nombreDeClase.classList.remove("topo");
-  })
-} 
+function cuadradoazar() {
+  cuadrado.forEach((nombredeclase) => {
+    nombredeclase.classList.remove("topo");
+  });
+
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
+  posicionalazar.classList.add("topo");
+
+  posiciontopo = posicionalazar.id;
+}
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContext = resultado;
+      posicionalazar = null;
+    }
+  });
+});
+function movertopo() {
+  tiempotopo = setInterval(cuadradoazar, 700);
+}
+movertopo();
+
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContext = tiempoactual;
+  if (tiempoactual === 0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert("Se acabó el tiempo, tu puntaje es" + resultado + "topos atrapados");
+  }
+}
+
+let idtiempo = setInterval(cuentaregresiva, 1000);
